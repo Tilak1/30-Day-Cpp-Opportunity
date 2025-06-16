3fabd897c33fe978d6e974f2b1f52d564e05fee2
Tilak Marupilla <tilakmarupilla67@gmail.com>
2025-06-15 23:06:31 -0400

    Update class_template_basic.cpp

--- Day 1/class_template_basic.cpp
+++ Day 1/class_template_basic.cpp
@@ -2,44 +2,31 @@
 using namespace std;
 
 template <typename T>
-
-
-Struct Pair {
-
+struct Pair {
 T x,y; // Assigning to local Template T from object's scope 
-        
 Pair (T i, T j)
 {
 x = i; 
 y = j; }
-
 T getFirst(); 
 T getSecond() 
-
 { return y; }
-
-
 }; 
 
 
 // Doing outside class function definition - so need to use the class name to access the functions
 // And the double use of template class again to delcare same T again
 
-T Pair<T>::het 
+template <typename T>
 
-template <typename T1>
-
-T1 Pair<T1>::getFirst()
+T Pair<T>::getFirst()
 {
     return x;
 }
-
-
-
 int main()
 
 {
 Pair<int> p1(10,20); 
-cout<<p1.getFirst()<<""<<pl.getSecond(); 
+cout<<p1.getFirst()<<"\t"<<p1.getSecond(); 
 return 0; 
 }
